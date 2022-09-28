7f9b8e620d3e8cdb8faca248db8571d8412fe055
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-09-28 15:56:06 +0700

    Initial commit

---
+++ Assets/Script/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NoiseMap {
+    public class MapGenerator : MonoBehaviour {
+
+        public int mapWidth;
+        public int mapHeight;
+        public float noiseScale;
+
+        public bool autoUpdate;
+
+        public void GenerateMap() {
+            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+            MapDisplay display = FindObjectOfType<MapDisplay>();
+            display.DrawNoiseMap(noiseMap);
+        }
+
+    }
+}
