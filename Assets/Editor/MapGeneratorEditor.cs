c9c6f47eaab8de1514e3c02ef9c134b067a15449
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-10-04 11:54:41 +0700

    feat: add ep12

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -11,12 +11,12 @@ public class MapGeneratorEditor : Editor {
 
         if (DrawDefaultInspector()) {
             if (mapGen.autoUpdate) {
-                mapGen.GenerateMap();
+                mapGen.DrawMapInEditor();
             }
         }
 
         if (GUILayout.Button("Generate")) {
-            mapGen.GenerateMap();
+            mapGen.DrawMapInEditor();
         }
     }
 }
