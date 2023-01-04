73eb2f05615e12bb0521cf3f99df93761d040861
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2023-01-04 16:24:07 +0700

    refractor: refractor blocky mesh generation code

---
+++ Assets/Script/ProceduralLandmassGeneration/Data/Minecraft/VoxelData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ProceduralLandmassGeneration.Data.Minecraft {
+    public static class VoxelData {
+        public static readonly Vector3[] DirectionsAroundBlock = {
+            new Vector3(0, 1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(0, 0, -1), //positive z direction
+            new Vector3(0, 0, 1), //negative z direction
+            new Vector3(1, 0, 0),
+            new Vector3(-1, 0, 0),
+        };
+
+        public static readonly Vector3[] VerticesLocalPos = {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, -1),
+            new Vector3(0, 0, -1),
+
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(1, 1, -1),
+            new Vector3(0, 1, -1)
+        };
+
+        public static readonly byte[,] VerticesIndicesOfFace = {
+            { 4, 5, 6, 7 }, //positive y direction
+            { 1, 0, 3, 2 }, //negative y direction
+            { 5, 4, 0, 1 }, //positive z direction
+            { 7, 6, 2, 3 }, //negative z direction
+            { 6, 5, 1, 2 }, //positive x direction
+            { 4, 7, 3, 0 }  //negative x direction
+        };
+    }
+}
--- Assets/Script/ProceduralLandmassGeneration/Generator/Mesh/BlockyMeshGenerator.cs
+++ Assets/Script/ProceduralLandmassGeneration/Generator/Mesh/BlockyMeshGenerator.cs
@@ -1,19 +1,69 @@
 ﻿using System.Collections.Generic;
 using ProceduralLandmassGeneration.Data;
+using ProceduralLandmassGeneration.Data.Minecraft;
 using UnityEngine;
 
 namespace ProceduralLandmassGeneration.Generator.Mesh {
     public class BlockyMeshGenerator : IMeshGenerator {
-        private int CHUNK_HEIGHT;
-        private int CHUNK_WIDTH;
+        private int _chunkHeight;
+        private int _chunkWidth;
         private int[,,] _blocks;
-        private int BASE_LAND_LEVEL;
+        private int _baseLandLevel;
+
+        public MeshData GenerateMeshData(float[,] heightMap, MeshSettings meshSettings, int levelOfDetail = 0) {
+            _chunkHeight = 64;
+            _chunkWidth = (int)meshSettings.MeshWorldSize + 2;
+            _blocks = new int[_chunkWidth, _chunkHeight, _chunkWidth];
+            _baseLandLevel = _chunkHeight / 2;
+
+            // cần phải build mesh từ +z -> -z để lấy đối xứng với noiseMap
+            Vector3 topLeftOfMesh = new Vector3(-1 * (_chunkWidth - 2) / 2, 0, 1 * (_chunkWidth - 2) / 2);
+
+            PopulateBlocksData(heightMap);
+
+            List<Vector3> verts = new List<Vector3>();
+            List<int> tris = new List<int>();
+            List<Vector2> uvs = new List<Vector2>();
+
+            for (int x = 1; x < _chunkWidth - 1; x++) {
+                for (int y = 0; y < _chunkHeight; y++) {
+                    for (int z = 1; z < _chunkWidth - 1; z++) {
+                        Vector3 currentBlockIndex = new Vector3(x, y, z);
+
+                        if (!IsTransparent(currentBlockIndex)) {
+                            // x,z - 1: because there are 2 extra blocks line in chunk width that are used to calculate block face
+                            // -(z-1) để lấy đối xứng với noise map
+                            Vector3 currentLocalPos = new Vector3(x - 1, y, -(z - 1));
+
+                            for (int direction = 0; direction < VoxelData.DirectionsAroundBlock.Length; direction++) {
+                                if (IsTransparent(currentBlockIndex + VoxelData.DirectionsAroundBlock[direction])) {
+                                    for (int index = 0; index < 4; index++) {
+                                        verts.Add(topLeftOfMesh +
+                                                  currentLocalPos +
+                                                  VoxelData.VerticesLocalPos[
+                                                      VoxelData.VerticesIndicesOfFace[direction, index]]);
+                                    }
+
+                                    int tl = verts.Count - 4;
+                                    tris.AddRange(new[] {
+                                        tl, tl + 1, tl + 2, tl, tl + 2, tl + 3
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            MeshData meshData = new MeshData(verts, tris);
+            return meshData;
+        }
 
         private void PopulateBlocksData(float[,] heightMap) {
-            for (int x = 0; x < CHUNK_WIDTH; x++) {
-                for (int y = 0; y < CHUNK_HEIGHT; y++) {
-                    for (int z = 0; z < CHUNK_WIDTH; z++) {
-                        if (y <= BASE_LAND_LEVEL + heightMap[x, z]) {
+            for (int x = 0; x < _chunkWidth; x++) {
+                for (int y = 0; y < _chunkHeight; y++) {
+                    for (int z = 0; z < _chunkWidth; z++) {
+                        if (y <= _baseLandLevel + heightMap[x, z]) {
                             _blocks[x, y, z] = 1;
                         } else {
                             _blocks[x, y, z] = 0;
@@ -23,127 +73,16 @@ namespace ProceduralLandmassGeneration.Generator.Mesh {
             }
         }
 
-        private bool IsTransparent(int blockType) {
-            return blockType == 0;
-        }
+        private bool IsTransparent(Vector3 index) {
+            int x = Mathf.FloorToInt(index.x);
+            int y = Mathf.FloorToInt(index.y);
+            int z = Mathf.FloorToInt(index.z);
 
-        public MeshData GenerateMeshData(float[,] heightMap, MeshSettings meshSettings, int levelOfDetail = 0) {
-            CHUNK_HEIGHT = 64;
-            CHUNK_WIDTH = (int)meshSettings.MeshWorldSize + 2;
-            _blocks = new int[CHUNK_WIDTH, CHUNK_HEIGHT, CHUNK_WIDTH];
-            BASE_LAND_LEVEL = CHUNK_HEIGHT / 2;
-
-            // cần phải build mesh từ +z -> -z để lấy đối xứng với noiseMap
-            Vector3 topLeftOfMesh =
-                new Vector3(-1 * (CHUNK_WIDTH - 2) / 2, 0, 1 * (CHUNK_WIDTH - 2) / 2);
-
-            PopulateBlocksData(heightMap);
-
-            List<Vector3> verts = new List<Vector3>();
-            List<int> tris = new List<int>();
-            List<Vector2> uvs = new List<Vector2>();
-
-            for (int x = 1; x < CHUNK_WIDTH - 1; x++) {
-                for (int y = 0; y < CHUNK_HEIGHT; y++) {
-                    for (int z = 1; z < CHUNK_WIDTH - 1; z++) {
-                        var currentBlockType = _blocks[x, y, z];
-
-                        if (!IsTransparent(currentBlockType)) {
-                            int numFaces = 0;
-
-                            //Because there are 2 extra blocks line in chunk width that are used to calculate block face
-                            int currentBlockLocalX = x - 1;
-                            int currentBlockLocalZ = -(z - 1);
-
-                            //world positive y direction
-                            if (y < CHUNK_HEIGHT - 1 && IsTransparent(_blocks[x, y + 1, z])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ - 1));
-                                numFaces++;
-                            }
-
-                            //world negative y direction
-                            if (y > 0 && IsTransparent(_blocks[x, y - 1, z])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh + new Vector3(currentBlockLocalX, y, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ - 1));
-                                numFaces++;
-                            }
-
-                            //world positive z direction
-                            if (IsTransparent(_blocks[x, y, z - 1])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh + new Vector3(currentBlockLocalX, y, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ));
-                                numFaces++;
-                            }
-
-                            //world negative z direction
-                            if (IsTransparent(_blocks[x, y, z + 1])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y, currentBlockLocalZ - 1));
-                                numFaces++;
-                            }
-
-                            //world positive x
-                            if (IsTransparent(_blocks[x + 1, y, z])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX + 1, y, currentBlockLocalZ - 1));
-                                numFaces++;
-                            }
-
-                            //world negative x
-                            if (IsTransparent(_blocks[x - 1, y, z])) {
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y + 1, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh + new Vector3(currentBlockLocalX, y, currentBlockLocalZ - 1));
-                                verts.Add(topLeftOfMesh +
-                                          new Vector3(currentBlockLocalX, y, currentBlockLocalZ));
-                                numFaces++;
-                            }
-
-                            int tl = verts.Count - 4 * numFaces;
-                            for (int i = 0; i < numFaces; i++) {
-                                tris.AddRange(new[] {
-                                    tl + i * 4, tl + i * 4 + 1, tl + i * 4 + 2, tl + i * 4, tl + i * 4 + 2,
-                                    tl + i * 4 + 3
-                                });
-                            }
-                        }
-                    }
-                }
+            if (x < 0 || x > _chunkWidth - 1 || y < 0 || y > _chunkHeight - 1 || z < 0 || z > _chunkWidth - 1) {
+                return true;
             }
 
-            MeshData meshData = new MeshData(verts, tris);
-            return meshData;
+            return _blocks[x, y, z] == 0;
         }
     }
 }
